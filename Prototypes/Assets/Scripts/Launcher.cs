dd30084d690bdd8a35224c5cdca446d744971901
Blake Leahy <leabl393@student.otago.ac.nz>
2024-01-24 16:23:37 +1300

    Launch script start - commit to test if push works in our repo

---
+++ Prototypes/Assets/Scripts/Launcher.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    Transform projectilePrefab;
+    Transform spawnpoint;
+    LineRenderer lineRenderer;
+
+    float launchForce = 2f;
+    float trajectoryTimeStep = 0.05f;
+    int trajectoryStepCount = 10;
+
+    void launch()
+    {
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
