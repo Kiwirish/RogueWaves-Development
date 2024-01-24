7108e8f650b4e8ee6e512a66a45764c6707c1a1a
Blake Leahy <leabl393@student.otago.ac.nz>
2024-01-24 18:32:07 +1300

    working projectile motion for shooting

--- Prototypes/Assets/Scripts/Launcher.cs
+++ Prototypes/Assets/Scripts/Launcher.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
-    Transform projectilePrefab;
-    Transform spawnpoint;
-    LineRenderer lineRenderer;
+    [SerializeField]Transform projectilePrefab;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] LineRenderer lineRenderer;
 
-    float launchForce = 2f;
-    float trajectoryTimeStep = 0.05f;
-    int trajectoryStepCount = 10;
+    [SerializeField] float launchForce = 2f;
+    [SerializeField] float trajectoryTimeStep = 0.05f;
+    [SerializeField] int trajectoryStepCount = 15;
 
-    void launch()
-    {
+    Vector2 velocity, startMousePosition, currentMousePosition;
 
-    }
-
-
+    
 
 
     // Start is called before the first frame update
@@ -29,6 +26,51 @@ public class Launcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            startMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            velocity = (startMousePosition - currentMousePosition) * launchForce;
+            DrawTrajectory();
+        }
+
+        if (Input.GetMouseButtonUp(0)){
+            ShootProjectile();
+            ClearTrajectory();
+        }
     }
+
+    void DrawTrajectory()
+    {
+        Vector3[] positions = new Vector3[trajectoryStepCount];
+        for(int i = 0; i < trajectoryStepCount; i++)
+        {
+            float timeElapsed = i * trajectoryTimeStep;
+            Vector3 position = (Vector2)spawnPoint.position + (velocity * timeElapsed) + (0.5f * Physics2D.gravity * timeElapsed * timeElapsed);
+            positions[i] = position;
+        }
+        lineRenderer.positionCount = trajectoryStepCount;
+        lineRenderer.SetPositions(positions);
+    }
+
+    void ClearTrajectory() {
+
+        lineRenderer.positionCount = 0;
+
+    }
+   
+
+    void ShootProjectile()
+    {
+        // create projectile prefab at spawnpoint 
+        Transform projectile = Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
+        // give it a velocity
+        projectile.GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+
+
 }
