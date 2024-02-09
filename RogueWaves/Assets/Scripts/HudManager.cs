b3b4d60c8790ba804e2327846c6970bd984f8406
Blake Leahy <leabl393@student.otago.ac.nz>
2024-02-09 17:01:24 +1300

    Crewmates HUD code started not functional

--- RogueWaves/Assets/Scripts/HudManager.cs
+++ RogueWaves/Assets/Scripts/HudManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class HudManager : MonoBehaviour
 {
@@ -8,11 +10,16 @@ public class HudManager : MonoBehaviour
     public GameObject pauseMenuPanel;
     public GameObject winMenuPanel;
     public GameObject loseMenuPanel;
+    public GameObject crewmateSelectionPanel;
+    public GameObject crewmateButtonPrefab; 
+    public Transform crewmateListParent; 
     public BattleSystem battleSystem;
 
     MenuManager pauseMenu;
     MenuManager winMenu;
     MenuManager loseMenu;
+    MenuManager crewmateSelectionMenu;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +34,9 @@ public class HudManager : MonoBehaviour
 
         loseMenu = loseMenuPanel.GetComponent<MenuManager>();
         loseMenu.Hide();
+
+        crewmateSelectionMenu = crewmateSelectionMenu.GetComponent<MenuManager>();
+        crewmateSelectionMenu.Hide();
 
     }
 
@@ -50,4 +60,54 @@ public class HudManager : MonoBehaviour
         }
 
     }
+
+    public void ShowCrewmateSelectionHUD()
+    {
+        // ClearCrewMateSelectionOptions();
+
+        foreach (var crewmateID in CrewManager.Instance.crewmatesGained)
+        {
+            CreateCrewmateSelectionButton(crewmateID);
+        }
+
+        crewmateSelectionMenu.Show();
+
+
+    }
+
+    public void CreateCrewmateSelectionButton(string crewmateID)
+    {
+        GameObject buttonInstance = Instantiate(crewmateButtonPrefab, crewmateListParent);
+
+        Text buttonText = buttonInstance.GetComponentInChildren<Text>();
+        if (buttonText != null)
+        {
+            buttonText.text = crewmateID; 
+        }
+
+        UnityEngine.UI.Button button = buttonInstance.GetComponent<UnityEngine.UI.Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(() => OnCrewmateSelected(crewmateID));
+        }
+    }
+
+    public void OnCrewmateSelected(string crewmateID)
+    {
+        Debug.Log("Crewmate selected: " + crewmateID);
+        
+        //CrewManager.Instance.ActivateCrewmatePowerUp(crewmateID);
+
+        crewmateSelectionMenu.Hide();
+    }
+
+    public void ClearCrewmateSelectionOptions()
+    {
+        foreach (Transform child in crewmateListParent)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+
 }
