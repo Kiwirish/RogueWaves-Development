f3bfb38671885b345450e46480f0615360343b7d
Blake Leahy <leabl393@student.otago.ac.nz>
2024-02-15 18:58:49 +1300

    Cut scene - animation, music

---
+++ RogueWaves/Assets/CutSceneController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutSceneController : MonoBehaviour
+{
+    public float cutsceneDuration = 30;
+
+    void Start()
+    {
+        StartCoroutine(EndCutsceneAfterTime(cutsceneDuration));
+    }
+
+    IEnumerator EndCutsceneAfterTime(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("MainMenu"); 
+    }
+}
